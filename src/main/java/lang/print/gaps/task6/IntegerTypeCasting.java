ecd898b0721bf4f99293aa2755227463bca47b84
Sarvarbek <112868538+sarvar1605@users.noreply.github.com>
2022-10-01 23:48:41 +0500

    Update IntegerTypeCasting.java

--- src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task6;
 
 public class IntegerTypeCasting {
     public static void main(String[] args) {
+        float num=3.4F;
+        System.out.println((int)num);
     }
 }
